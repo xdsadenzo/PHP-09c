efac36f995cc3e0cd54fd19fbb4484bfc469d405
sajid <sh273720@gmail.com>
2024-07-22 12:50:42 +0500

    php first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$name ="php Enzo";
+// embeding
+// echo $name;
+$num1 = 13;
+$num2 = 9;
+//  echo $addresult = $num1 + $num2;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo $name ?>
+</h1>
+    <h2> <?php echo $num1 + $num2 ?></h2>
+    <h2> <?php echo $num1 - $num2 ?></h2>
+    <h2> <?php echo $num1 * $num2 ?></h2>
+    <h2> <?php echo $num1 / $num2 ?></h2>
+</body>
+</html>
